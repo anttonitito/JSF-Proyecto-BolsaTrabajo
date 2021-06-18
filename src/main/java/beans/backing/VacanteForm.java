8d7b2eea78f618a531a41f6725d466da186f4574
Antoni <antonitm96@gmail.com>
2021-06-18 11:35:39 -0500

    Implementando Managed Beans y Backing Bean en JSF

---
+++ src/main/java/beans/backing/VacanteForm.java
@@ -0,0 +1,36 @@
+package beans.backing;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import beans.model.Candidato;
+
+@Named
+@RequestScoped
+public class VacanteForm {
+
+	@Inject
+	private Candidato candidato;
+
+	public Candidato getCandidato() {
+		return candidato;
+	}
+
+	public void setCandidato(Candidato candidato) {
+		this.candidato = candidato;
+	}
+	
+	public String enviar() {
+		if (this.candidato.getNombre().equals("Juan")) {
+			
+			return "exito";
+			
+		}else {
+			return "fallo";
+		}
+	}
+	
+	
+	
+}
